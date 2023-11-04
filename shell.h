517bc733153fada21b1cc5116f9079b5fa45f275
Tobechi Duru <tobechi.duru001@gmail.com>
2023-11-04 10:54:22 -0700

    Shell.h

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+/* Buffers */
+#define BUFF_SIZE 1024
+#define TOKEN_SIZE 64
+/*Functions samples */
+void shell(void);
+char *_getline(void);
+char **pase_line(char *);
+int _getchar(void);
+void *_relloc(void *, unsigned int, unsigned int);
+char *_udicpy(char *, char *, unsigned int);
+char **strt(char *);
+int _straff(char *);
+#endif
