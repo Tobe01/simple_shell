aad402a7aa42d6ba3c6f39cee024388c52c2d1bb
Tobechi Duru <tobechi.duru001@gmail.com>
2023-11-04 10:34:32 -0700

    Main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: indicates arg count
+ * @av: indicates arg vector
+ *
+ * Return: 0 on success,and 1 on error
+ */
+int main(void)
+{
+shell()
+populate_env_list(info);
+read_history(info);
+hsh(info);
+return (EXIT_SUCCESS);
+}
+
