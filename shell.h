2ad266727ee2713023f97f371600ab888050a61c
Tobechi Duru <tobechi.duru001@gmail.com>
2023-11-04 11:45:16 -0700

    Shell.h

--- shell.h
+++ shell.h
@@ -1,12 +1,15 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 /* Buffers */
 #define BUFF_SIZE 1024
 #define TOKEN_SIZE 64
+
 /*Functions samples */
 void shell(void);
 char *_getline(void);
